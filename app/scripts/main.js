d91bcdfb80d5f77be76fa4da0215e53f51f0d8fb
Sebastian Schepis <sschepis@gmail.com>
2014-06-12 00:49:17 -0700

    .

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,7 @@ var BOOKEND = {bookend:true,word:false};
 
 function VisualText(data) {
     this.word = data.trim();
+    this.index = 0;
     this.nextWord = BOOKEND;
     this.prevWord = BOOKEND;
     if(this.word.indexOf(',') !== -1)
@@ -22,8 +23,8 @@ VisualText.split = function(textToDisplay, splbnd) {
     var tarray = textToDisplay.split(splbnd);
     for(var el in tarray) {
         el = tarray[el];
-        if(!el || el === '') continue;
         displayOut.push( new VisualText(el) );
+        displayOut[displayOut.length-1].index = displayOut.length-1;
         if(displayOut.length > 1) {
             displayOut[displayOut.length - 2].nextWord = displayOut[displayOut.length - 1];
             displayOut[displayOut.length - 1].prevWord = displayOut[displayOut.length - 2];
@@ -32,7 +33,9 @@ VisualText.split = function(textToDisplay, splbnd) {
     return displayOut.length > 0 ? displayOut[0] : BOOKEND;
 };
 
-VisualText.prototype.play = function(callback) {
+VisualText.prototype.play = function(timeout, callback, complete) {
+    var timeoutOverride =  callback ? timeout : -1;
+    callback = callback ? callback : timeout;
     var start = this;
     if(!start || start.bookend )
         return;
@@ -41,9 +44,12 @@ VisualText.prototype.play = function(callback) {
         if(visibility) {
             setTimeout(function(){
                 playWord(word, false, callback);
-            }, word.pause);
+            }, timeoutOverride != -1 ? timeoutOverride : word.pause);
         } else {
-            playWord(word.nextWord, true, callback);
+            if(word) playWord(word.nextWord, true, callback);
+            else {
+                if(complete) complete(null);
+            }
         }
     };
     playWord(start, true, callback);
@@ -52,8 +58,6 @@ VisualText.prototype.play = function(callback) {
 /*
  **********************************
  */
-
-var texttext = "A German man committed to a high-security psychiatric hospital after being accused of fabricating a story of money-laundering activities at a major bank is to have his case reviewed after evidence has emerged proving the validity of his claims.";
 
 var pause = true;
 var firstWord = BOOKEND;
@@ -70,6 +74,7 @@ var updateWordsOnPage = function(word, visibility) {
     currentWord = word;
     var out = visibility ? word.word : '';
     $('#blinkreader span.blink').html(out);
+
     var ret = visibility ? !pause : true;
     if(!word.bookend && word.nextWord.bookend && visibility) {
         ret = false;
@@ -82,6 +87,23 @@ var updateWordsOnPage = function(word, visibility) {
     }
     return ret;
 };
+
+var recreateText = function(theWord) {
+    var outputText = '';
+    $('#output').html('');
+    theWord.play(0,
+        function(word, visibility) {
+            if(visibility && word && word.word) {
+                outputText += '<span style="float:left" id="word' + word.index + '">' + word.word + '&nbsp;</span>';
+                $('#output').append('<span style="float:left" id="word' + word.index + '">' + word.word + '&nbsp;</span>');
+            }
+            if(!visibility && word && word.nextWord && word.nextWord.bookend)
+                $('#output').html(outputText);
+            return true;
+        });
+};
+
+
 
 $('#slower').click(function() {
 });
@@ -109,6 +131,7 @@ $('#go-button').click(function() {
     var address = $('#location').val();
     $.get('http://127.0.0.1:8080/alchemy/content?' + address, function(data) {
         currentWord = firstWord = VisualText.split(data.text.text);
+        recreateText(currentWord);
         currentWord.play(updateWordsOnPage);
     });
 });
@@ -120,6 +143,10 @@ $(document).ready(function() {
     $("#blinkreader")
         .draggable()
         .resizable();
+    $("#output")
+        .draggable()
+        .resizable();
     $("#blinkreader span.blink")
         .fitText();
+    //$('#output').flowtype();
 });
